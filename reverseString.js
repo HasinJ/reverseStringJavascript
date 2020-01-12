a0baa65b253625d0d3a292960d629c32270ab6a0
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-12 01:26:13 -0500

    third test still works - made it easier to understand (with comments and moving "let letters" outside the for loop

--- reverseString.js
+++ reverseString.js
@@ -1,10 +1,10 @@
 const reverseString = function(sentence) {
   let words = sentence.split(' ');
   let newSentence = [];
+  let letters = [];
 
   for (let i = words.length-1; i >= 0; i--) {
     let newWord = []; //to get rid of the remains of the last iteration
-    let letters = [];
     letters = words[i].split('');
 
     for (let j = letters.length-1; j >= 0; j--) {
@@ -13,7 +13,7 @@ const reverseString = function(sentence) {
     }
     newSentence.push(newWord.join(''));
     /*
-      since we're starting with the last word, 
+      since we're starting with the last word,
       pushing that word into the new sentence reverses the order instead of writing words[i]
     */
   }
